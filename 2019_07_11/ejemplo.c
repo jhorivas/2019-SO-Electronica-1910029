a4d9429744b32167ff1cc5226987c1c7c261927b
miguel cardona <mcardona.complex@gmail.com>
2019-07-11 12:11:39 -0500

    Adicionando archivos de clase

---
+++ 2019_07_11/ejemplo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>                                                                              
+                                                                                                 
+void demo(char *x, char* z){                                                                     
+ *x = 'b';                                                                                       
+ *z = 'c';                                                                                       
+}                                                                                                
+                                                                                                 
+int main(int argc, char** argv) {                                                                
+  char z = 'a';                                                                                  
+  char *y;                                                                                       
+                                                                                                 
+  y = (char*)malloc(sizeof(char));                                                               
+  demo(&z,y);                                                                                    
+  printf("%c %c\n",z,*y);                                                                        
+                                                                                                 
+  return 0;
+}
